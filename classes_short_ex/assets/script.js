1c54a4ec51488acb39b1b40c3fd7fc37f1a8645a
Petúnia Dias <147607484+petuniadias@users.noreply.github.com>
2024-06-29 21:36:35 +0100

    last changes

--- classes_short_ex/assets/script.js
+++ classes_short_ex/assets/script.js
@@ -4,4 +4,13 @@ class escapeRoom {
     image = "";
     solved = "false";
     level = 0;
+
+    constructor(id, title, image, solved, level) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.solved = solved;
+        this.level = level;
+    }
 }
+
