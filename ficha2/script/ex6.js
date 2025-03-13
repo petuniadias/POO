c03b827527b38c124aaec16c8725ccce660c2cca
Petunia <psofia20056@gmail.com>
2025-03-13 20:11:07 +0000

    Função que descobre a quantidade de vezes que uma palavra aparece num texto

--- ficha2/script/ex6.js
+++ ficha2/script/ex6.js
@@ -1,6 +1,19 @@
+function findWord(texto, palavraDePesquisa) {
+  const text = texto.split(" ");
+  let wordCount = 0;
+
+  for (i = 0; i < text.length; i++) {
+    if(text[i] === palavraDePesquisa) {
+      wordCount++
+    }
+  }
+  return wordCount;
+}
+
 const texto = prompt();
 const palavraDePesquisa = prompt();
 
-function findWord() {
-  
-}
+console.log(texto);
+console.log(palavraDePesquisa);
+
+console.log(findWord(texto, palavraDePesquisa));
