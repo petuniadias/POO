5a21d3873a6d521bbbca4f48e998978504b392ed
Petunia <psofia20056@gmail.com>
2025-03-14 12:23:22 +0000

    Soma do intervalo entre 2 números

--- ficha1/script/ex1.js
+++ ficha1/script/ex1.js
@@ -1,10 +1,16 @@
 //ex. 1
-const num1 = parseInt(prompt('Indique o primeiro número'));
-const num2 = parseInt(prompt('Indique o segundo número'));
+let num1 = parseInt(prompt('Indique o primeiro número'));
+let num2 = parseInt(prompt('Indique o segundo número'));
 
 console.log(num1, num2);
 
   //caso o num1 for sup ao num2 tem de funcionar
+
+if(num1 > num2) {
+  let temp = num1;
+  num1 = num2;
+  num2 = temp;
+}
 
 let soma = 0;
 
