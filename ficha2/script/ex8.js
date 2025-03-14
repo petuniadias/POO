d75ccb50b1d0988156bc852c8c6b9b109d5d1d68
Petunia <psofia20056@gmail.com>
2025-03-14 11:15:30 +0000

    Função que calcula o fatorial de um número

---
+++ ficha2/script/ex8.js
@@ -0,0 +1,22 @@
+const promptNumber = document.querySelector('.o-tal-do-prompt');
+const submitBtn = document.querySelector('.submit-btn');
+const fatorialMsg = document.querySelector('.fatorial-msg');
+const number = document.querySelector('.number');
+
+
+submitBtn.addEventListener('click', () => {
+  const num = parseInt(promptNumber.value);
+
+  if(isNaN(num) || num < 0) {
+    fatorialMsg.innerHTML = `Insira um número válido!`;
+    fatorialMsg.classList.add('no-value');
+    return;
+  } else {
+    let fatorial = 1;
+    for(let i = 1; i <= num; i++) {
+      fatorial *= i;
+    }
+    fatorialMsg.style.display = `flex`;
+    number.innerHTML = `${fatorial}`;
+  }
+});
