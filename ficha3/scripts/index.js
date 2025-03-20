7ae4ce108f27b66b3a49179d1b19b147be83d2b0
Petúnia <psofia20056@gmail.com>
2025-03-20 15:51:57 +0000

    Outras propriedades e funçao que diz o número de propriedades dentro do objeto "compra"

--- ficha3/scripts/index.js
+++ ficha3/scripts/index.js
@@ -1,6 +1,7 @@
 const submit = document.querySelector('.submit')
 const produtos = document.querySelector('.produtos');
 
+let compra;
 
 submit.addEventListener('click', () => {
     const nomeProduto = document.querySelector('.nomeProduto').value;
@@ -18,16 +19,31 @@ submit.addEventListener('click', () => {
 
     linha.appendChild(nomeProdutoResultado);
     linha.appendChild(catProdutoResultado);
-    
-    const compra = {
+
+    compra = {
         nomeProduto,
-        catProduto
+        catProduto,
+        precoBase: 2.50,
+        txIva: 23,
+        precoFinal: function() {
+            return precoBase * txIva;
+        },
+        dadosEncomenda: {
+            dataEncomenda: '2023-03-27',
+            prazoEntrega: '(7) dias',
+            localEntrega: 'Avenida do Mar, 74, 4460-810 Matosinhos' 
+        }
     };
 
     nomeProdutoResultado.innerHTML = `${compra.nomeProduto}`;
     catProdutoResultado.innerHTML = `${compra.catProduto}`;
-    
+    showLength();
 });
+
+function showLength() {
+    console.log(Object.keys(compra).length);
+}
+
 
 
 
