b1edd16724a6e8bc1b8fc4817ff4fcc2a4687e66
Petúnia <psofia20056@gmail.com>
2025-03-20 15:24:09 +0000

    Formulário e lista de produtos

---
+++ ficha3/scripts/index.js
@@ -0,0 +1,33 @@
+const submit = document.querySelector('.submit')
+const produtos = document.querySelector('.produtos');
+
+
+submit.addEventListener('click', () => {
+    const nomeProduto = document.querySelector('.nomeProduto').value;
+    const catProduto = document.querySelector('.catProduto').value;
+
+    const linha = document.createElement('div');
+    linha.classList.add('linha');
+    produtos.appendChild(linha);
+
+    const nomeProdutoResultado = document.createElement('div');
+    nomeProdutoResultado.classList.add('nomeProduto-resultado');
+
+    const catProdutoResultado = document.createElement('div');
+    catProdutoResultado.classList.add('catProduto-resultado');
+
+    linha.appendChild(nomeProdutoResultado);
+    linha.appendChild(catProdutoResultado);
+    
+    const compra = {
+        nomeProduto,
+        catProduto
+    };
+
+    nomeProdutoResultado.innerHTML = `${compra.nomeProduto}`;
+    catProdutoResultado.innerHTML = `${compra.catProduto}`;
+    
+});
+
+
+
