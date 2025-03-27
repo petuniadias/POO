f1a07b51e368e8e99626ec50c77447fb45ce4900
Petunia <psofia20056@gmail.com>
2025-03-27 15:44:56 +0000

    Gestor de jogos de computador - formulário e tabela

---
+++ ficha5/scripts/index.js
@@ -0,0 +1,25 @@
+let games = []
+const form = document.querySelector('.add-game-forms');
+
+form.addEventListener('submit', event => {
+  event.preventDefault(); //prevenir/suspender executar a açao por omissão de enviar os dados para o servidor
+
+  const name = document.querySelector('game-name').value;
+  const year = document.querySelector('year').value;
+  const genre = document.querySelector('genre').value;
+  const rating = document.querySelector('.rating').value;
+
+  const plataforms =  [...document.querySelector('.plataform')];
+
+  const thisYear = new Date().getFullYear();
+
+  if (year > thisYear) {
+    alert('');
+    return;
+  }
+
+  const newGame = {name, year, genre, plataforms, rating};
+  games.push(newGame);
+  console.log(games);
+  form.reset();
+});
