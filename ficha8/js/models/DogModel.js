9e78598f089c69864aea59122c2ec5febfa156ef
Petúnia <psofia20056@gmail.com>
2025-07-14 19:52:57 +0100

    ex1 ficha 1

---
+++ ficha8/js/models/DogModel.js
@@ -0,0 +1,2 @@
+// Definir a classe Dog com os atributos do enunciado
+class Dog()
---
+++ ficha8/js/views/CatalogDogView.js
@@ -0,0 +1,8 @@
+// Importa o modulo init.js
+import * as data from "../init.js";
+// declara array 
+let dogs = []
+// invoca função init (módulo init.js) para preencher array de objetos com tasks pré-definidas
+dogs = data.init();
+
+// Renredirzar o catálogo com o array de objetos
